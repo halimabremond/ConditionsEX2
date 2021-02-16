a4b940f3b0c421d0f85c977b296303c96e856112
halimabremond <halima.bremond@yahoo.com>
2021-02-16 13:49:45 +0100

    conditions ex2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConditionsEX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("veuillez entrer age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+            {
+                Console.WriteLine("Vous avez {0} ans, vous êtes donc mineur.e.", age);
+            }
+            else
+            {
+                Console.WriteLine("Vous avez {0} ans, vous êtes donc majeur.e.", age);
+            }
+        }
+    }
+}
